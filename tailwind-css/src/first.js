408039a917d0b4ad6787c774ab03999ff2adc6e9
Kamlesh <kamleshpandey25k@gmail.com>
2026-04-14 14:34:28 +0530

    react-with tailwind

---
+++ tailwind-css/src/first.js
@@ -0,0 +1,201 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-wrap justify-center items-center min-h-screen bg-gray-100 gp-20">
+        
+        <div className="max-w-sm overflow-hidden bg-white shadow-xl rounded-2xl p-4 text-center">
+          
+          <img
+            className="w-full h-70 object-cover rounded-2xl"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzyQ04AzdUZruFEu6q8yw-1ji4zkSN9EPPchP7fHThOotwrtasOUkkSow&s"
+            alt="Profile"
+          />
+
+          <div className="mt-4 font-bold text-2xl text-gray-800">
+            <h1>About Me</h1>
+          </div>
+
+          <div className="mt-2 text-gray-600">
+            <p>Rampal Sir is very handsome</p>
+          </div>
+
+          <div>
+            <button className="mt-4 text-white px-6 py-3 rounded-2xl bg-black hover:bg-blue-500 transition duration-300 shadow-lg">
+              Know More
+            </button>
+          </div>
+
+        </div>
+
+         <div className="max-w-sm overflow-hidden bg-white shadow-xl rounded-2xl p-4 text-center">
+          
+          <img
+            className="w-full h-70 object-cover rounded-2xl"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzyQ04AzdUZruFEu6q8yw-1ji4zkSN9EPPchP7fHThOotwrtasOUkkSow&s"
+            alt="Profile"
+          />
+
+          <div className="mt-4 font-bold text-2xl text-gray-800">
+            <h1>About Me</h1>
+          </div>
+
+          <div className="mt-2 text-gray-600">
+            <p>Rampal Sir is very handsome</p>
+          </div>
+
+          <div>
+            <button className="mt-4 text-white px-6 py-3 rounded-2xl bg-black hover:bg-blue-500 transition duration-300 shadow-lg">
+              Know More
+            </button>
+          </div>
+
+        </div>
+         <div className="max-w-sm overflow-hidden bg-white shadow-xl rounded-2xl p-4 text-center">
+          
+          <img
+            className="w-full h-70 object-cover rounded-2xl"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzyQ04AzdUZruFEu6q8yw-1ji4zkSN9EPPchP7fHThOotwrtasOUkkSow&s"
+            alt="Profile"
+          />
+
+          <div className="mt-4 font-bold text-2xl text-gray-800">
+            <h1>About Me</h1>
+          </div>
+
+          <div className="mt-2 text-gray-600">
+            <p>Rampal Sir is very handsome</p>
+          </div>
+
+          <div>
+            <button className="mt-4 text-white px-6 py-3 rounded-2xl bg-black hover:bg-blue-500 transition duration-300 shadow-lg">
+              Know More
+            </button>
+          </div>
+
+        </div>
+         <div className="max-w-sm overflow-hidden bg-white shadow-xl rounded-2xl p-4 text-center">
+          
+          <img
+            className="w-full h-70 object-cover rounded-2xl"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzyQ04AzdUZruFEu6q8yw-1ji4zkSN9EPPchP7fHThOotwrtasOUkkSow&s"
+            alt="Profile"
+          />
+
+          <div className="mt-4 font-bold text-2xl text-gray-800">
+            <h1>About Me</h1>
+          </div>
+
+          <div className="mt-2 text-gray-600">
+            <p>Rampal Sir is very handsome</p>
+          </div>
+
+          <div>
+            <button className="mt-4 text-white px-6 py-3 rounded-2xl bg-black hover:bg-blue-500 transition duration-300 shadow-lg">
+              Know More
+            </button>
+          </div>
+
+        </div>
+         <div className="max-w-sm overflow-hidden bg-white shadow-xl rounded-2xl p-4 text-center">
+          
+          <img
+            className="w-full h-70 object-cover rounded-2xl"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzyQ04AzdUZruFEu6q8yw-1ji4zkSN9EPPchP7fHThOotwrtasOUkkSow&s"
+            alt="Profile"
+          />
+
+          <div className="mt-4 font-bold text-2xl text-gray-800">
+            <h1>About Me</h1>
+          </div>
+
+          <div className="mt-2 text-gray-600">
+            <p>Rampal Sir is very handsome</p>
+          </div>
+
+          <div>
+            <button className="mt-4 text-white px-6 py-3 rounded-2xl bg-black hover:bg-blue-500 transition duration-300 shadow-lg">
+              Know More
+            </button>
+          </div>
+
+        </div>
+         <div className="max-w-sm overflow-hidden bg-white shadow-xl rounded-2xl p-4 text-center">
+          
+          <img
+            className="w-full h-70 object-cover rounded-2xl"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzyQ04AzdUZruFEu6q8yw-1ji4zkSN9EPPchP7fHThOotwrtasOUkkSow&s"
+            alt="Profile"
+          />
+
+          <div className="mt-4 font-bold text-2xl text-gray-800">
+            <h1>About Me</h1>
+          </div>
+
+          <div className="mt-2 text-gray-600">
+            <p>Rampal Sir is very handsome</p>
+          </div>
+
+          <div>
+            <button className="mt-4 text-white px-6 py-3 rounded-2xl bg-black hover:bg-blue-500 transition duration-300 shadow-lg">
+              Know More
+            </button>
+          </div>
+
+        </div>
+         <div className="max-w-sm overflow-hidden bg-white shadow-xl rounded-2xl p-4 text-center">
+          
+          <img
+            className="w-full h-70 object-cover rounded-2xl"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzyQ04AzdUZruFEu6q8yw-1ji4zkSN9EPPchP7fHThOotwrtasOUkkSow&s"
+            alt="Profile"
+          />
+
+          <div className="mt-4 font-bold text-2xl text-gray-800">
+            <h1>About Me</h1>
+          </div>
+
+          <div className="mt-2 text-gray-600">
+            <p>Rampal Sir is very handsome</p>
+          </div>
+
+          <div>
+            <button className="mt-4 text-white px-6 py-3 rounded-2xl bg-black hover:bg-blue-500 transition duration-300 shadow-lg">
+              Know More
+            </button>
+          </div>
+
+        </div>
+         <div className="max-w-sm overflow-hidden bg-white shadow-xl rounded-2xl p-4 text-center">
+          
+          <img
+            className="w-full h-70 object-cover rounded-2xl"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzyQ04AzdUZruFEu6q8yw-1ji4zkSN9EPPchP7fHThOotwrtasOUkkSow&s"
+            alt="Profile"
+          />
+
+          <div className="mt-4 font-bold text-2xl text-gray-800">
+            <h1>About Me</h1>
+          </div>
+
+          <div className="mt-2 text-gray-600">
+            <p>Rampal Sir is very handsome</p>
+          </div>
+
+          <div>
+            <button className="mt-4 text-white px-6 py-3 rounded-2xl bg-black hover:bg-blue-500 transition duration-300 shadow-lg">
+              Know More
+            </button>
+          </div>
+
+        </div>
+
+      </div>
+    </>
+  );
+}
+
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
